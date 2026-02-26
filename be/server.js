b6b6e8d924ca633fd60851e1b500d626408c87dd
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2026-02-26 19:29:11 +0700

    add manage blog admin

--- be/server.js
+++ be/server.js
@@ -1,30 +1,29 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.join(__dirname, ".env") });
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const blogRoute = require("./routes/blogRoute");
 
-dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to Database
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/blogs", blogRoute);
 
 app.get("/", (req, res) => {
-    res.send("API is running...");
+  res.send("API is running...");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,7 +1,48 @@
-import type { NextConfig } from "next";
+const path = require("path");
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
+    ],
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "",
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || "",
+  },
+  webpack(config, options) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+
+    // 👇 Disable overlay if using Webpack
+    if (options.dev && !options.isServer) {
+      config.devServer = {
+        ...(config.devServer || {}),
+        client: {
+          overlay: false,
+        },
+      };
+    }
+
+    return config;
+  },
+
+  // 👇 Tắt Turbopack
+  experimental: {
+    turbo: false,
+  },
+  devIndicators: false,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
