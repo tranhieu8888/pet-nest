a034b43730a97705084e44e272a5c299495c3cff
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2026-03-07 16:35:26 +0700

    .

--- fe/src/app/admin/voucher/page.tsx
+++ fe/src/app/admin/voucher/page.tsx
@@ -410,7 +410,6 @@ export default function VoucherPage() {
                 setIsCreateDialogOpen(true);
               }}
             >
-              <Plus className="mr-2 h-4 w-4" />
               Thêm Mới
             </Button>
           </div>
