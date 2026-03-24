65bf8cfe50b9cfa8feac079f3d9fa2323f358891
HieuTran <adstranhieu18@gmail.com>
2026-03-24 17:24:21 +0700

    fix homepage

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         {clientId ? (
           <GoogleOAuthProvider clientId={clientId}>
